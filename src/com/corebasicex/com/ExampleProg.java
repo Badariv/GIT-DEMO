9c24cbc9e62a93b4b5b22432f8a46bf340b1367e
Madhavi <madhavi.pulivarthi@gmail.com>
2024-03-27 07:02:17 +0530

    commit GitDemo

---
+++ src/com/corebasicex/com/ExampleProg.java
@@ -0,0 +1,12 @@
+package com.corebasicex.com;
+
+public class ExampleProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("A sample program......");
+
+	}
+
+}
